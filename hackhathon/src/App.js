89a852485a8e5870a4f2e9d4bde39cc98a08afa6
veenasahu12 <veenasahusbp@gmail.com>
2022-07-28 19:40:43 +0530

    App.js

--- hackhathon/src/App.js
+++ hackhathon/src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import SIGNIN from './pages/SIGNIN/SIGNIN';
 import SIGNUP from './pages/SIGNUP/SIGNUP';
-import Navbar from './pages/Navbar';
 import Video from './pages/MainPage/Video';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      
          <Routes>
            <Route path='/' element={<SIGNUP />}/>
            <Route path='/signin' element={<SIGNIN />}/>
